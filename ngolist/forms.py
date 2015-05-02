d83e3ac9cbae229f1214897321c0a70ce4e7873b
new-player <siddharthjhanwar@yahoo.in>
2015-05-03 00:35:16 +0530

    Added filter to remove data with no website url.

--- ngolist/forms.py
+++ ngolist/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from haystack.forms import SearchForm
-
+from haystack.inputs import Raw
 
 class StateSelectSearchForm(SearchForm):
     state = forms.CharField(label='State', required=False)
@@ -8,11 +8,15 @@ class StateSelectSearchForm(SearchForm):
     def search(self):
         # First, store the SearchQuerySet received from other processing.
         sqs = super(StateSelectSearchForm, self).search()
-
+        sqs = sqs.exclude(ngo_url = Raw("[* TO *]"))
+        #sqs = sqs.exclude(Website_Url__isnull = True).exclude(Website_Url__exact='')
+        
         if not self.is_valid():
             return self.no_query_found()
 
         if self.cleaned_data['state']:
             sqs = sqs.filter(state=self.cleaned_data['state'])
+            
+            
 
         return sqs
